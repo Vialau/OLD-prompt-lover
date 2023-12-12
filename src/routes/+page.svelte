6e5b259f325de0e745d01d6398d04af78ea5c0b6
Vianney LAURENT <vianney.lau@gmail.com>
2023-12-12 11:05:53 +0100

    Add feature page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,11 +11,11 @@
 		  </defs>
 		</svg>
 		<div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-		  <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Boostez votre productivité<br>Utilisez Prompt Lover dès maintenant</h2>
+		  <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Prompt Lover<br>Votre base de prompt</h2>
 		  <p class="mt-6 text-lg leading-8 text-gray-300">Avec Prompt Lover, votre créativité n'a plus de limite.</p>
 		  <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
 			<a href="/pricing" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">S'abonner</a>
-			<a href="#" class="text-sm font-semibold leading-6 text-white">Plus d'info <span aria-hidden="true">→</span></a>
+			<a href="/features" class="text-sm font-semibold leading-6 text-white">Plus d'info <span aria-hidden="true">→</span></a>
 		  </div>
 		</div>
 		<div class="relative mt-16 h-80 lg:mt-8">
---
+++ src/routes/features/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="p-4">
+	<h1>Features</h1>
+	<p class="mt-4 capitalize">Page détail des fonctionnalités</p>
+</div>
